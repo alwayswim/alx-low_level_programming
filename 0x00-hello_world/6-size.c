dafb592afbb35623e7c30a12b16b5ceb706dca95
alwayswim <alwayswim@gmail.com>
2021-08-26 14:27:07 -0700

    size of type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * void: no parameters
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art, \n");
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+peerf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
+}
