b692f811917d38a9673b841f37bacea65d4fc2d6
alwayswim <alwayswim@gmail.com>
2021-08-26 14:52:39 -0700

    checking Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - main function
+ * main - Entry point
  * void: no parameters
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
