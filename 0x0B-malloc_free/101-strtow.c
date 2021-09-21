c0f9f45112a0c03671c9ed3feffacd57df6b4433
alwayswim <alwayswim@gmail.com>
2021-09-21 13:02:57 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,64 +2,68 @@
 #include <stdlib.h>
 
 /**
- * copychars - copies chars to buffer
- * @b: destination buffer
- * @start: starting char pointer
- * @stop: ending char pointer
-*/
-void copychars(char *b, char *start, char *stop)
+ * wrdcnt - counts the number of words in a string
+ * @s: string to count
+ *
+ * Return: int of number of words
+ */
+int wrdcnt(char *s)
 {
-while (start <= stop)
-*b++ = *start++;
-*b = 0;
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
 }
 
 /**
- * wordcount - counts the number of words
- * @str: the sentence string
+ * strtow - splits a string into words
+ * @str: string to split
  *
- * Return: int number of words
-*/
-int wordcount(char *str)
-{
-int words = 0, in_word = 0;
-
-while (1)
-{
-if (*str == ' ' || !*str)
-{
-if (in_word)
-words++;
-in_word = 0;
-if (!*str)
-break;
-}
-else
-in_word++;
-str++;
-}
-return (words);
-}
-
-/**
- * strtow - splits sentence into words
- * @str: the sentence string
- *
- * Return: pointer to string array
-*/
+ * Return: pointer to an array of strings
+ */
 char **strtow(char *str)
 {
-int words = 0, in_word = 0;
-char **ret, *word_start;
+	int i, j, k, l, n = 0, wc = 0;
+	char **w;
 
-if (*str == ' ' || !*str)
-{
-if (in_word)
-{
-ret[words] = malloc(sizeof(char) * (in_word + 1));
-if (!ret[words])
-if (!str || !*str || !wordcount(str))
-return (NULL);
-ret = malloc(sizeof(char *) * (wordcount(str) + 1));
-while (1)
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	n = wrdcnt(str);
+	if (n == 1)
+		return (NULL);
+	w = (char **)malloc(n * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w[n - 1] = NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(w[k]);
+				free(w[n - 1]);
+				free(w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w[wc][l] = str[i + l];
+			w[wc][l] = '\0';
 
