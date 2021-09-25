b1a1bcad357d648e0ac084f27041db1c059bb568
alwayswim <alwayswim@gmail.com>
2021-09-25 13:32:36 -0700

    edited static library

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+ unsigned int _strspn(char *s, char *accept);
+    return (0);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+ char *_strstr(char *haystack, char *needle);
+    return (0);
+}
