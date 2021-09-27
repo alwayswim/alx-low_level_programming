14622d8f580c3ac848b24abadb92cf907a91a4fe
alwayswim <alwayswim@gmail.com>
2021-09-27 16:18:42 -0700

    print the name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the program
+ * main - Print the name of the file compiled from
  * Return: 0 if success
  */
+
 int main(void)
 {
-	printf("%s \n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
