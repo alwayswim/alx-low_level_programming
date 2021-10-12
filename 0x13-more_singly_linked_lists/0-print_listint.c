8a48b2609a2ed8aa9eeb061c7677c660821634ce
alwayswim <alwayswim@gmail.com>
2021-10-12 15:09:23 -0700

    print_listint_edited

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- * print_listint - prints a linked list
- * @h: the head of the linked list
- * Return: the length of the string
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of elements
  */
 size_t print_listint(const listint_t *h)
 {
-	register int len = 0;
+	register size_t len = 0;
 
 	while (h)
 	{
-		(!h->n)
-		? printf("null\n")
-      		: printf("%d\n", h->n);  
-	      	h = h->next;
-	       	len++;
+		printf("%i\n", h->n);
+		h = h->next;
+		len++;
 	}
 	return (len);
 }
